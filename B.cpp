1f30cfdb81a3fc412f3746eb31d421f9f0625a0e
Rohit Grover <rohit.grover@arm.com>
2015-07-28 11:39:23 +0100

    changes after reviewing B.cpp

--- B.cpp
+++ B.cpp
@@ -13,54 +13,39 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 #include "mbed.h"
 #include "BLE.h"
-#define DUMP_ADV_DATA 0
 
-#define ASSERT_NO_FAILURE(X)  (X == BLE_ERROR_NONE) ? (printf("{{success}}\r\n")) : printf("{{failure}} %s at line %u\r\n", #X, __LINE__);
-#define CHECK_EQUALS(X,Y) ((X)==(Y)) ? (printf("{{sucess}}\n")) : printf("{{failure}}\n");
+#define ASSERT_NO_FAILURE(X) (X == BLE_ERROR_NONE) ? (printf("{{success}}\r\n")) : printf("{{failure}} %s at line %u\r\n", #X, __LINE__);
+#define CHECK_EQUALS(X,Y)    ((X) == (Y)) ? (printf("{{sucess}}\r\n")) : printf("{{failure}}\r\n");
 
-BLE        ble;
-uint8_t address[6];
+BLE ble;
+Gap::Address_t peerAddress;
 
-
-
-void advertisementCallback(const Gap::AdvertisementCallbackParams_t *params) {
-    for (int i = 0; i < 5; i++){
-        if(address[i] != params->peerAddr[i]){
-            return;    
+void advertisementCallback(const Gap::AdvertisementCallbackParams_t *params)
+{
+    for (size_t i = 0; i < Gap::ADDR_LEN; i++) {
+        if (peerAddress[i] != params->peerAddr[i]) {
+            return; /* Filter away adverts all adverts except from the targeted peer. */
         }
     }
-    printf("ScanResp: %u, Data: %d\r\n", params->isScanResponse, *(params->advertisingData + params->advertisingDataLen -1));
-//    printf("Adv peerAddr: [%02x %02x %02x %02x %02x %02x] rssi %d, ScanResp: %u, AdvType: %u, Data: %d\r\n",
-//           params->peerAddr[5], params->peerAddr[4], params->peerAddr[3], params->peerAddr[2], params->peerAddr[1], params->peerAddr[0],
-//           params->rssi, params->isScanResponse, params->type, *(params->advertisingData + params->advertisingDataLen -1));
-           
-//    printf("Adv peerAddr: [%d %d %d %d %d %d] rssi %d, ScanResp: %u, AdvType: %u\r\n",
-//           params->peerAddr[5], params->peerAddr[4], params->peerAddr[3], params->peerAddr[2], params->peerAddr[1], params->peerAddr[0],
-//          params->rssi, params->isScanResponse, params->type);
-           
-#if DUMP_ADV_DATA
-    for (unsigned index = 0; index < params->advertisingDataLen; index++) {
-        printf("%02x ", params->advertisingData[index]);
-    }
-    printf("\r\n");
-#endif /* DUMP_ADV_DATA */
-}
 
+    /* emit the last byte of the advert payload. */
+    printf("ScanResp: %u, Data: %u\r\n", params->isScanResponse, *(params->advertisingData + params->advertisingDataLen - 1));
+}
 
 int main(void)
 {
-    printf("{{success}}" "\n" "{{end}}" "\n");
-    for (int i = 0; i < 6; i++){
-        scanf("%hhu",&address[i]);    
+    printf("{{success}}\r\n{{end}}\r\n"); /* to handover control from the hosttest to the python script. */
+
+    /* Read in the peer address. */
+    for (unsigned i = 0; i < Gap::ADDR_LEN; i++) {
+        scanf("%hhu", &peerAddress[i]);
     }
-    
+
     ASSERT_NO_FAILURE(ble.init());
 
-    ASSERT_NO_FAILURE(ble.gap().setScanParams(500 /* scan interval */, 200 /* scan window */, 0, true));
+    ASSERT_NO_FAILURE(ble.gap().setScanParams(500 /* scan interval */, 200 /* scan window */, 0, true /* active scanning */));
     ASSERT_NO_FAILURE(ble.gap().startScan(advertisementCallback));
-
- 
 }
